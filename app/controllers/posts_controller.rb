2c4f6119eb4b564e29fbb773a5d4bc9a58e2b591
ichikawa-riku <owner@M15-108.local>
2019-04-03 08:28:35 +0900

    edit posts_controller
    index new createアクションを定義

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,24 @@
 class PostsController < ApplicationController
+  def index
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Group.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+private
+  def post_params
+    params.require(:post).permit(:title, :content).merge(id: current_user.id)
+  end
+
 end
+
